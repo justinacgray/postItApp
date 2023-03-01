2af7f8574b4e481525fd330e8f979ca37a1a677d
Justina Gray <justinacgray@gmail.com>
2023-02-28 22:22:40 -0500

    added styles

--- client/src/views/Dashboard.jsx
+++ client/src/views/Dashboard.jsx
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import {Navigate, useParams} from 'react-router-dom'
+import { Navigate, useParams, Link } from 'react-router-dom'
 import '../css/Dashboard.css'
 import AllNotes from '../components/AllNotes'
 import NewNote from '../components/NewNote'
@@ -10,7 +10,7 @@ const Dashboard = () => {
 
   const [modalToggle, setModalToggle] = useState(false)
   const [leftOpen, setLeftOften] = useState(false)
-  
+
   // const openModalToggle = () => {
   //   alert("Clicked")
   // }
@@ -19,25 +19,35 @@ const Dashboard = () => {
 
   }
 
-  
-  
-  return (
-    <div id='layout'>
-      <aside className='sidebar'>
-        <div>
-          <button onClick={() => setModalToggle(true)}>
-            Let's Post Something
-          </button>
-        {modalToggle && <NewNote closeModal={setModalToggle} /> }
-        </div>
-      </aside>
-      <main className='main-body'>
-        <AllNotes />
-        {/* <EditNote /> */}
-      </main>
 
+
+  return (
+    <div className='dash-body'>
+      <nav>
+        <input type="checkbox" name="" id="side-menu-switch" />
+        <div className="side-menu">
+          <form action="">
+            <input type="text" placeholder="Search For Note" />
+            <button>
+              <i className="fas fa-search"></i>
+            </button>
+          </form>
+          <Link to="" onClick={() => setModalToggle(true)}><i className="fas fa-gavel"></i>Create a Note!</Link>
+          <label htmlFor="side-menu-switch">
+            <i className="fas fa-angle-left"></i>
+          </label>
+        </div>
+      {modalToggle && <NewNote closeModal={setModalToggle} /> }
+      </nav>
+      <main >
+      <AllNotes  />
+      {/* <EditNote /> */}
+
+      </main>
+      
     </div>
   )
 }
 
 export default Dashboard;
+
