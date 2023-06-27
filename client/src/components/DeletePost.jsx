5eba93947f294f7db9c82981b873b86bcdbde495
Justina <justinacgray@gmail.com>
2023-06-26 21:46:38 -0400

    added deletePost file - pending

---
+++ client/src/components/DeletePost.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const DeletePost = () => {
+
+
+  // const removeFromDom = (postsId) => {
+  //     setPostsArray(postsArray.filter(post => posts._id !== postsId));
+  // }
+
+  // const deleteProduct = (postsId) => {
+  //     axios.delete("http://localhost:7000/api/delete/")
+  //         .then(res => {
+  //             removeFromDom(postsId);
+  //             // navigate upon success 
+  //         })
+  //         .catch((err) => {
+  //             console.log(err);
+  //         })
+  //     }
+
+    return (
+    <div>DeletePost</div>
+    )
+}
+
+export default DeletePost
